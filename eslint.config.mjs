f074f85bf4f3e816858c81cd1719492bee05d448
Nishant Kohli <nishkohli96@gmail.com>
2025-11-06 18:49:55 +0530

    v2.0.0 - Upgrade Next.js to v16 (#8)
    upgrade nextjs to v16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 import jsConfig from '@nish1896/eslint-flat-config/js';
 import nextConfig from '@nish1896/eslint-flat-config/next';
 import nextTsConfig from '@nish1896/eslint-flat-config/next-ts';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   ...jsConfig,
   ...nextConfig,
   ...nextTsConfig,
-  {
-    rules: {
-      '@typescript-eslint/triple-slash-reference': 'off',
-    }
-  }
-];
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
 export default eslintConfig;
